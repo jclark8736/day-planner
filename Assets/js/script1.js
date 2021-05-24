e4438ff7472218599095a16f825b81e373980f47
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-24 19:01:38 -0400

    containers working

--- Assets/js/script1.js
+++ Assets/js/script1.js
@@ -2,7 +2,7 @@
 //global variables
 
 var currentDay = moment().format("MM Do YYYY");
-var currentTime = moment().format("MM DO YYYY hh:mm")
+var currentTime = moment().format("MM D YYYY hh:mm")
 var testTime=document.getElementById("time-display")
 //display code on page load
 
